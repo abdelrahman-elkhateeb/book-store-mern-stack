9249e5439a28b22642110c3c6bc7049cabaf71fa
abdelrahman elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-10-20 15:27:13 +0300

    create drop down

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+const category = [
+  "choose a genre",
+  "business",
+  "fiction",
+  "horror",
+  "adventure",
+];
+
 function TopSellers() {
-  return <div>top seller</div>;
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+  return (
+    <div className="py-10">
+      <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
+      {/* category filtering */}
+      <div className="mb-8 flex item-center">
+        <select
+          name="category"
+          id="category"
+          className="border bg-[#eaeaea] border-gray-300 rounded-md px-4 py-2 focus:outline-none"
+        >
+          {category.map((category, i) => {
+            return (
+              <option key={i} value={category}>
+                {category}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default TopSellers;
