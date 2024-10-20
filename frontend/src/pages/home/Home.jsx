8bb34b638217b4dc8311f84e6fbe4e8a1b798c04
abdelrahman elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-10-20 16:28:18 +0300

    top seller Patrion

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import Banner from "./Banner";
 import TopSellers from "./TopSellers";
-
+import Recommended from "./Recommended";
 function Home() {
   return (
     <>
       <Banner />
       <TopSellers />
+      <Recommended />
     </>
   );
 }
--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,4 +1,15 @@
 import { useEffect, useState } from "react";
+import BookCard from "../books/BookCard";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
 const category = [
   "choose a genre",
   "business",
@@ -9,11 +20,25 @@ const category = [
 
 function TopSellers() {
   const [books, setBooks] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("choose a genre");
+
   useEffect(() => {
     fetch("books.json")
       .then((res) => res.json())
       .then((data) => setBooks(data));
   }, []);
+
+  const filteredBooks =
+    selectedCategory === "choose a genre"
+      ? books
+      : books.filter(
+          (book) => book.category === selectedCategory.toLowerCase(),
+        );
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
   return (
     <div className="py-10">
       <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
@@ -23,16 +48,53 @@ function TopSellers() {
           name="category"
           id="category"
           className="border bg-[#eaeaea] border-gray-300 rounded-md px-4 py-2 focus:outline-none"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
         >
-          {category.map((category, i) => {
+          {category.map((cat, i) => {
             return (
-              <option key={i} value={category}>
-                {category}
+              <option key={i} value={cat}>
+                {cat}
               </option>
             );
           })}
         </select>
       </div>
+
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        modules={[Navigation]}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1280: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        className="mySwiper"
+      >
+        {filteredBooks.length > 0 &&
+          filteredBooks.map((book, i) => (
+            <SwiperSlide key={i}>
+              <BookCard book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* display filtered books */}
     </div>
   );
 }
