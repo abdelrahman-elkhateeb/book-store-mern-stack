c9a764e9b1ded6167e724e22ce659dc1a3a293f7
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-12-17 22:09:11 +0200

    Update BookCard.jsx

--- frontend/src/pages/books/BookCard.jsx
+++ frontend/src/pages/books/BookCard.jsx
@@ -2,7 +2,7 @@ import { FiShoppingCart } from "react-icons/fi";
 import { getImgUrl } from "../../utils/getImgUrl";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../../redux/features/cart/cartSlice";
+import { addToCart } from "../../redux/features/cart/CartSlice";
 function BookCard({ book }) {
   const dispatch = useDispatch();
 
