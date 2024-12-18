e23e85dd09c276197114e116b642bf54a09e7d91
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-12-18 22:29:17 +0200

    Update baseURL.js

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-  return "https://book-store-mern-stack-backend-tau.vercel.app";
+  return "https://book-store-mern-stack-backend-one.vercel.app";
 };
 
 export default getBaseUrl;
